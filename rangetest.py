99d3431cce881ad2b2b5262e3efd6fff63ee576d
jrahman1988 <jamilur_rahman@yahoo.com>
2021-03-01 00:14:23 -0500

    commit

--- rangetest.py
+++ rangetest.py
@@ -2,3 +2,6 @@ from pyspark.shell import spark
 
 myRange = spark.range(1000).toDF("number")
 myRange.show()
+
+strings = spark.read.text("file:///mnt/sda/Spark/spark-3.0.1-bin-hadoop3.2/README.md")
+strings.show(10, truncate=False)
