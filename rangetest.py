61a9474692608e80914b7cb17dd6d1b49e6779e0
jrahman1988 <jamilur_rahman@yahoo.com>
2021-03-01 00:21:56 -0500

    commit

--- rangetest.py
+++ rangetest.py
@@ -1,7 +1,10 @@
 from pyspark.shell import spark
 
+strings = spark.read.text("file:///mnt/sda/Spark/spark-3.0.1-bin-hadoop3.2/README.md")
+strings.show(10, truncate=False)
+
 myRange = spark.range(1000).toDF("number")
 myRange.show()
 
-strings = spark.read.text("file:///mnt/sda/Spark/spark-3.0.1-bin-hadoop3.2/README.md")
-strings.show(10, truncate=False)
+divisBy2 = myRange.where("number % 2 = 0")
+divisBy2.show()
